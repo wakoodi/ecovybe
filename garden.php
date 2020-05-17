c7476ee64befe88eaea298156b9b64826e56dfe9
Amber <amber.waltens@hotmail.com>
2020-05-17 13:15:38 +0200

    navigatie toegevoegd

--- garden.php
+++ garden.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Tuin</title>
 </head>
 <body>
-    I'm the home page
+    I'm the garden page
     <?php include("includes/nav.php") ?>
 </body>
 </html>
---
+++ includes/nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+    <div>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="garden.php">Tuin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="profile.php">Profiel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="info.php">Info</a>
+            </li>
+        </ul>
+    </div>
+</nav>
