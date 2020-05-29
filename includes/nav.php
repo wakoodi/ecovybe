b44c978120e6efbafb1983280a37044fbfe7f2f0
Amber <amber.waltens@hotmail.com>
2020-05-29 11:47:10 +0200

    icon as link

--- includes/nav.php
+++ includes/nav.php
@@ -2,20 +2,16 @@
     <div>
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <img src="public/icons/plant.png" alt="plant icon">
-                <a class="nav-link" href="garden.php">Tuin</a>
+                <a class="nav-link" href="garden.php"><img src="public/icons/plant.png" alt="plant icon">Tuin</a>
             </li>
             <li class="nav-item">
-                <img src="public/icons/home.png" alt="home icon">
-                <a class="nav-link" href="home.php">Home</a>
+                <a class="nav-link" href="home.php"><img src="public/icons/home.png" alt="home icon">Home</a>
             </li>
             <li class="nav-item">
-                <img src="public/icons/profile.png" alt="profile icon">
-                <a class="nav-link" href="profile.php">Profiel</a>
+                <a class="nav-link" href="profile.php"><img src="public/icons/profile.png" alt="profile icon">Profiel</a>
             </li>
             <li class="nav-item">
-                <img src="public/icons/logout.png" alt="logout icon">
-                <a class="nav-link" href="info.php">Afmelden</a>
+                <a class="nav-link" href="info.php"><img src="public/icons/logout.png" alt="logout icon">Afmelden</a>
             </li>
         </ul>
     </div>
