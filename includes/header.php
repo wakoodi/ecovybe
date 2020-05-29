e504b74bd264f004e2e204d791ab37286b4aecf9
Amber <amber.waltens@hotmail.com>
2020-05-29 11:57:52 +0200

    icons added to header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <a href="home.php"><<</a>
+    <a href="home.php"><img src="public/icons/back.png" alt="back icon"></a>
     <a href="home.php"><img class="logo" src="public/images/logo.png" alt="logo ecovybe"></a>
-    <a href="notification.php">🔔</a>
+    <a href="notification.php"><img src="public/icons/notification.png" alt="notification icon"></a>
 </div>
