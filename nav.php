6c0b687179b6a629686b1da4dc1383ac93329b1a
Lisa_Drse <55496129+r0423168@users.noreply.github.com>
2020-06-11 00:51:36 +0200

    added css
    CSS added to index.php, home.php, header.php, nav.php, advice.php, login.php

---
+++ nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+    <div>
+        <ul class="nav">
+            <li class="nav-itemA">
+                <a class="nav-link" href="garden.php"><img class="gardenicon" src="public/icons/plant.png" alt="plant icon"><p class="grden">Tuin</p></a>
+            </li>
+            <li class="nav-itemB">
+                <a class="nav-link" href="home.php"><img class="homeicon" src="public/icons/home.png" alt="home icon"><p class="home">Home</p></a>
+            </li>
+            <li class="nav-itemC">
+                <a class="nav-link" href="profile.php"><img class="proficon" src="public/icons/profile.png" alt="profile icon"><p class="prof">Profiel</p></a>
+            </li>
+            <li class="nav-itemD">
+                <a class="nav-link" href="logout.php"><img class="outicon" src="public/icons/logout.png" alt="logout icon"><p class="out">Afmelden</p></a>
+            </li>
+        </ul>
+    </div>
+</nav>
