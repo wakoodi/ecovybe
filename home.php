7009ca1ae101559363fa53a4cf0bffb2da78ef9a
LisaDrse <r0423168@student.thomasmore.be>
2020-06-11 11:26:03 +0200

    branch lisa

--- home.php
+++ home.php
@@ -22,13 +22,13 @@ if (isset($_SESSION['user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css" />
+<!--     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->    <link rel="stylesheet" href="css/style.css" />
+    <link rel = 'stylesheet' href = 'css/style.css'>
     <title>Home</title>
 </head>
-<body>
+<body class="container">
     <?php include("includes/header.php") ?>
-    <h1>Welkom <?php echo htmlspecialchars($info['firstName'])?> !</h1>
+    <h1 class="welcome">Welkom <?php echo htmlspecialchars($info['firstName'])?> !</h1>
     <div class="gardens">
         <?php foreach ($gardens as $garden) : ?>
            <?php $gardenId = $garden['id'];
@@ -39,17 +39,17 @@ if (isset($_SESSION['user'])) {
             <div class="singleGarden">
                 <p> 
                     <img src="<?php echo $item['pic_url']?>" alt="product">
-                    <h2><?php echo htmlspecialchars($garden['name'])?></h2>
-                    <?php echo $item['name']?>
+                    <h2 class="gardenname"><?php echo htmlspecialchars($garden['name'])?></h2>
+                    <p class="gardenname"><?php echo $item['name']?></p>
                 </p>
-                <a href="advice.php">Advies</a>
+                <a class="advicelink" href="advice.php" class="advice">Advies</a>
             </div>
         </a>
         <?php endforeach; ?>
     </div>
     <?php include("includes/nav.php") ?>
 </body>
-<script src = 'https://code.jquery.com/jquery-3.4.1.slim.min.js' integrity = 'sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n' crossorigin = 'anonymous'></script>
+<!-- <script src = 'https://code.jquery.com/jquery-3.4.1.slim.min.js' integrity = 'sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n' crossorigin = 'anonymous'></script>
     <script src = 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity = 'sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin = 'anonymous'></script>
-    <script src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity = 'sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin = 'anonymous'></script>
+    <script src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity = 'sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin = 'anonymous'></script> -->
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <a href="home.php"><img src="public/icons/back.png" alt="back icon"></a>
-    <a href="home.php"><img class="logo" src="public/images/logo.png" alt="logo ecovybe"></a>
-    <a href="notification.php"><img src="public/icons/notification.png" alt="notification icon"></a>
+    <a href="home.php"><img class="back" src="public/icons/back.png" alt="back icon"></a>
+    <a href="home.php"><img class="logoheader" src="public/images/logo.png" alt="logo ecovybe"></a>
+    <a href="notification.php"><img class="notification" src="public/icons/notification.png" alt="notification icon"></a>
 </div>
