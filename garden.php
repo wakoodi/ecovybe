e8cbc8295c6fd7905f7d9a32ca08a64915f8391b
Amber <amber.waltens@hotmail.com>
2020-06-12 11:55:40 +0200

    dropdown fixed

--- garden.php
+++ garden.php
@@ -36,7 +36,6 @@ if (isset($_SESSION['user'])) {
             <label for="items">Kies wat je zal kweken</label></br>
                 <select name="items" class="form-control">
                     <?php foreach ($items as $item){
-                        $item->setGardenId($item['id']);
                         echo "<option value=$item>$item</option>";
                     }?>
                 </select>
@@ -52,3 +51,4 @@ if (isset($_SESSION['user'])) {
     <script src = 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity = 'sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin = 'anonymous'></script>
     <script src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity = 'sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin = 'anonymous'></script>
 </html>
+
