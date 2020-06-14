21b3163d6dec866c007a1c8c88ca48f994780032
LisaDrse <r0423168@student.thomasmore.be>
2020-06-14 11:28:16 +0200

    correction css

--- includes/headerno.php
+++ includes/headerno.php
@@ -1,3 +1,3 @@
-<div class="header">
-    <a href="home.php"><img class="logoheader" src="public/images/logo.svg" alt="logo ecovybe"></a>
+<div class="headerno">
+    <a href="home.php"><img class="logoheaderno" src="public/images/logo.svg" alt="logo ecovybe"></a>
 </div>
